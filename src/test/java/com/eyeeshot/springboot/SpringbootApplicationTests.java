cb5e8d842c56ddfe17d6e75f0151ed4ceddf0002
eyeeshot <eyeeshot@gmail.com>
2022-05-23 14:00:18 +0900

    init

---
+++ src/test/java/com/eyeeshot/springboot/SpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eyeeshot.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
